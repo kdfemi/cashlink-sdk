1a89494cf045f7baf94d3e93475a84839351ed0e
Femi Kolade <koladefemi@gmail.com>
2022-10-04 14:19:56 +0100

    corrected buildUrl

--- src/utils/queryBuilder.ts
+++ src/utils/queryBuilder.ts
@@ -8,14 +8,14 @@ import { config } from "../config";
  */
 export function buildUrl(path: string, props: Record<string, string | number> = {})  {
     const hasFirstSlash = path.indexOf('/') === 0;
-    const url = (localStorage.getItem('CASH_LINK_ENV') === 'dev' ? config.staging_url : config.url) + (hasFirstSlash ? '' : '/') + path + '?origin='  + config.origin;
     let query = '&';
     for(const key in props) {
       if(path.includes(`:${key}`)) {
-        path = url.replace(`:${key}`, encodeURIComponent(props[key]));
+        path = path.replace(`:${key}`, encodeURIComponent(props[key]));
       } else {
         query += encodeURIComponent(key) + '=' + encodeURIComponent(props[key]) + '&'; 
       }
     }
+    const url = (localStorage.getItem('CASH_LINK_ENV') === 'dev' ? config.staging_url : config.url) + (hasFirstSlash ? '' : '/') + path + '?origin='  + config.origin;
     return url + query.replace(/&$/g, '');
 }
