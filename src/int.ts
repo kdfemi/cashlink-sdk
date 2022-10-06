f44c6bcf033eefa750c38a2c664b9bf35f1dab75
Femi Kolade <koladefemi@gmail.com>
2022-10-06 10:06:46 +0100

    quick push

--- src/int.ts
+++ src/int.ts
@@ -13,10 +13,12 @@ const init = (isDev = false) => {
     if(!isSSR()) {
         if(cashLinkInit) {
             window.removeEventListener('message', onMessageHandler)
+            window.removeEventListener('popstate', onPopStateHandler)
         }
         cashLinkInit = true;
     
         window.addEventListener('message', onMessageHandler, false)
+        window.addEventListener('popstate', onPopStateHandler, false)
     }
     if(isDev) {
         localStorage.setItem('CASH_LINK_ENV', 'dev')
@@ -29,8 +31,18 @@ const init = (isDev = false) => {
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const onMessageHandler = (e: MessageEvent<any>) => {
     if(e.data === config.closeEvent) {
+        window.removeEventListener('popstate', onPopStateHandler);
         closeModal();
     }
+};
+
+const onPopStateHandler = (_e: PopStateEvent) => {
+    if(document.getElementById(config.modalId)) {
+        history.go(1);
+    }
+    // if(e.data === config.closeEvent) {
+    //     closeModal();
+    // }
 };
 
 export default init;
--- src/utils/iframeBuilder.ts
+++ src/utils/iframeBuilder.ts
@@ -14,6 +14,7 @@ const iFrameBuilder = (src: string, showFrame = true) => {
     iframe.style.backgroundColor = 'transparent';
     const modal = document.createElement('div');
     modal.style.position = 'fixed'
+    modal.style.zIndex = '99999999'
     modal.style.left = '0';
     modal.style.right = '0';
     modal.style.top = '0';
@@ -25,6 +26,22 @@ const iFrameBuilder = (src: string, showFrame = true) => {
     if(showFrame) {
         const body = document.getElementsByTagName('body')[0];
         body.appendChild(modal);
+        const div = document.createElement('div'),
+        ref = document.getElementsByTagName('base')[0] || 
+              document.getElementsByTagName('script')[0];
+              div.innerHTML = '&shy;<style> iframe { visibility: hidden; } </style>';
+
+            ref.parentNode?.insertBefore(div, ref);
+
+        
+        /*
+            2. When window loads, remove that CSS, 
+            making iframe visible again
+        */
+        
+        iframe.onload = function() {
+            div.parentNode?.removeChild(div);
+        }
     }
     return modal;
     
